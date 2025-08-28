885d8657a6d1d6c07e50998c13e672511e130d48
pedro.vieira <pedro.vieira@fcxlabs.com.br>
2025-08-28 11:17:23 -0300

    feat: Criado mapeamento e ajuste na entidade a partir da tebela

--- src/Services/ECommerce.StockService/3-Infrastructure/ECommerce.StockService.Infrastructure.Data/Mappings/ProductMapping.cs
+++ src/Services/ECommerce.StockService/3-Infrastructure/ECommerce.StockService.Infrastructure.Data/Mappings/ProductMapping.cs
@@ -9,14 +9,45 @@ public class ProductMapping : IEntityTypeConfiguration<Product>
     public void Configure(EntityTypeBuilder<Product> builder)
     {
         builder.HasKey(p => p.Id);
-        builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
-        builder.Property(p => p.Description).HasColumnType("").HasMaxLength(500);
-        builder.Property(p => p.Price).IsRequired().HasColumnType("decimal(18,2)").HasColumnName("Price");
-        builder.Property(p => p.StockQuantity).IsRequired().HasColumnType("").HasColumnName("StockQuantity");
-        builder.Property(p => p.CreatedAt).IsRequired().HasDefaultValueSql("GETDATE()");
-        builder.Property(p => p.UpdatedAt).IsRequired().HasDefaultValueSql("GETDATE()");
-        builder.Property(p => p.IsActive).IsRequired().HasDefaultValue(true);
 
-        builder.ToTable("Products");
+        builder.Property(p => p.Id)
+            .IsRequired()
+            .HasColumnType("serial4")
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasColumnType("varchar(255)")
+            .HasColumnName("nome")
+            .HasMaxLength(255);
+
+        builder.Property(p => p.Description)
+            .HasColumnType("text")
+            .HasColumnName("descricao")
+            .IsRequired(false);
+
+        builder.Property(p => p.Price)
+            .IsRequired()
+            .HasColumnType("numeric(10,2)")
+            .HasColumnName("preco");
+
+        builder.Property(p => p.StockQuantity)
+            .IsRequired()
+            .HasColumnType("int4")
+            .HasColumnName("quantidade_estoque")
+            .HasDefaultValue(0);
+
+        builder.Property(p => p.CreatedAt)
+            .HasColumnType("timestamp")
+            .HasColumnName("criado_em")
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+        builder.Property(p => p.UpdatedAt)
+            .HasColumnType("timestamp")
+            .HasColumnName("atualizado_em")
+            .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+        builder.ToTable("produtos", schema: "public");
     }
 }
