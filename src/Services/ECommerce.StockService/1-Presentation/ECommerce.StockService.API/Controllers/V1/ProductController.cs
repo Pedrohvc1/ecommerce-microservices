887b331117d25e222275c4714c1df156472867a7
pedro.vieira <pedro.vieira@fcxlabs.com.br>
2025-08-30 16:45:59 -0300

    create doc the endpoint

--- src/Services/ECommerce.StockService/1-Presentation/ECommerce.StockService.API/Controllers/V1/ProductController.cs
+++ src/Services/ECommerce.StockService/1-Presentation/ECommerce.StockService.API/Controllers/V1/ProductController.cs
@@ -5,6 +5,13 @@ namespace ECommerce.StockService.API.Controllers.V1;
 
 public class ProductController : BaseApiController
 {
+    /// <summary>
+    /// Creates a new product.
+    /// </summary>
+    /// <param name="command">Command containing the necessary data to create the product.</param>
+    /// <returns>
+    /// Returns HTTP 200 (OK) status with the result of the product creation.
+    /// </returns>
     [HttpPost]
     public async Task<IActionResult> CreateAsync([FromBody] CreateProductCommandRequest command)
     {
